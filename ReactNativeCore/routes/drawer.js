ce91834b381059b2e8718e6c9e5239bb169a88d8
PandaPalumbo <mattdpalumbo@gmail.com>
2020-05-05 20:17:41 -0400

    settings added to drawer

--- ReactNativeCore/routes/drawer.js
+++ ReactNativeCore/routes/drawer.js
@@ -4,6 +4,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import HomeStack from '../routes/homeStack';
 import AboutStack from '../routes/aboutStack';
 import MapStack from '../routes/mapStack';
+import SettingsTab from '../Screens/SettingsTab';
 import theme from '../Styles/theme';
 import { ThemeColors } from 'react-navigation';
 import { color } from 'react-native-reanimated';
@@ -22,15 +23,19 @@ function CustomDrawerContent(props){
 export default function Navigator() {
   return (
     <NavigationContainer>
-      <Drawer.Navigator drawerContentOptions={
-        {
+      <Drawer.Navigator drawerContentOptions={{
           activeTintColor:theme.LIGHT_PINK,
           inactiveTintColor:theme.LIGHT_PINK
-        }
-      } drawerStyle={{backgroundColor:theme.DARK}} initialRouteName="Home" overlayColor="#20232A" >
+        }} 
+        drawerStyle={{
+          backgroundColor: theme.DARK
+        }} 
+        initialRouteName="Home" 
+        overlayColor="#20232A" >
         <Drawer.Screen  name="Home" component={HomeStack} />
         <Drawer.Screen name="About" component={AboutStack} />
         <Drawer.Screen name="Map" component={MapStack} /> 
+        <Drawer.Screen name="Settings" component={SettingsTab} /> 
       </Drawer.Navigator>
     </NavigationContainer>
   );
