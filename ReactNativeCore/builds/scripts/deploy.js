c0d613c6c10064ba42ba7777d607bb323d350501
Derrick Montgomery <dderrickmatheww@gmail.com>
2021-08-12 23:39:25 -0400

    Configure EAS Build

--- ReactNativeCore/builds/scripts/deploy.js
+++ ReactNativeCore/builds/scripts/deploy.js
@@ -1,7 +1,8 @@
-const { spawn } = require("child_process");
 const myArgs = process.argv.slice(2);
 const arg1 = myArgs[0];
 const arg2 = myArgs[1];
+const _utils = require('./utils');
+const utils = new _utils();
 const deployObj = {
     OperatingSystem: arg2.toLocaleLowerCase(),
 }
@@ -10,37 +11,10 @@ switch (arg1) {
     case 'deploy':
         switch (deployObj.OperatingSystem) {
             case 'ios':
-                let expoBuild = spawn("expo", [`build:${deployObj.OperatingSystem}`]);
-                expoBuild.stdout.on("data", data => {
-                    console.log(`stdout: ${data}`);
-                });
-                expoBuild.stderr.on("data", data => {
-                    console.log(`stderr: ${data}`);
-                });
-                expoBuild.on('error', (error) => {
-                    console.log(`error: ${error.message}`);
-                });
-                expoBuild.on("close", code => {
-                    console.log(`child process exited with code ${code}`);
-                });
+                utils.functions.build.expo_build({ isApple: true });
                 break;
             case 'android':
-                let expoBuild = spawn("expo", [`build:${deployObj.OperatingSystem}`]);
-                expoBuild.stdout.on("data", data => {
-                    console.log(`stdout: ${data}`);
-                });
-                
-                expoBuild.stderr.on("data", data => {
-                    console.log(`stderr: ${data}`);
-                });
-                
-                expoBuild.on('error', (error) => {
-                    console.log(`error: ${error.message}`);
-                });
-                
-                expoBuild.on("close", code => {
-                    console.log(`child process exited with code ${code}`);
-                });
+                utils.functions.build.expo_build({ isApple: false });
                 break;
             default:
                 console.log(`
--- ReactNativeCore/builds/scripts/utils.js
+++ ReactNativeCore/builds/scripts/utils.js
@@ -9,18 +9,20 @@ class Utils {
             build: {
                 expo_build: async (isApple) => {
                     this.state.isApple = isApple;
-                    console.log('Build started!');
                     const cwd = process.cwd();
-                    const build = isApple ? `expo build:ios` : `expo build:android`;
-                    const submit = `eas submit --platform=ios --path=${ isApple ? path.resolve('../watcher/', 'Payload.ipa') : path.resolve('../watcher/', 'Payload.apk')}`;
-                    await this.functions.build.exec({ build, cwd });
-                    const answer = await utils.functions.mesc.askQuestion({
+                    const cmd =  isApple ? `expo build:ios -t archive` : `expo build:android -t apk`;
+                    this.functions.mesc.consoleLog({ msg: 'Build started!' });
+                    this.functions.mesc.loader();
+                    //await this.functions.build.exec({ cmd, cwd });
+                    this.functions.mesc.clearLoader();
+                    const answer = await this.functions.mesc.askQuestion({
                         type: 'input',
                         name: 'isFileMoved',
                         message: "Please move the build file produced by expo to the file path MobileAppDevelopment/build/watcher! Enter 'y' or 'yes' when finished!"
                     });
                     if (answer.isFileMoved.toLowerCase() === 'yes' || answer.isFileMoved.toLowerCase() === 'y') {
-                        await this.functions.build.exec({ submit, cwd });
+                        const cmd = `eas submit --platform=${ isApple ? 'ios' : 'android' } --path=${ isApple ? path.resolve('../watcher/ipa', 'Payload.ipa') : path.resolve('../watcher/apk', 'Payload.apk')}`
+                        await this.functions.build.exec({ cmd, cwd });
                     }
                     else if (answer.isFileMoved.toLowerCase() === 'no' || answer.isFileMoved.toLowerCase() === 'n') {
                         return false;
@@ -31,9 +33,14 @@ class Utils {
                     }
                 },
                 exec: async ({ cmd, cwd }) => { 
-                    const { stdout, stderr } = await exec(cmd, { "cwd":  cwd });
-                    console.log(stdout);
-                    console.log(stderr);
+                    try {
+                        const { stdout, stderr } = await exec(cmd, { "cwd":  cwd });
+                        console.log(stdout);
+                        console.log(stderr);
+                    }
+                    catch (err) {
+                        this.functions.mesc.consoleLog({ msg: err });
+                    }
                 }
             },
             deploy: {
@@ -48,10 +55,28 @@ class Utils {
                             message: message,
                         }
                     ]);
+                },
+                consoleLog: ({ msg }) => {
+                    console.log(`
+${ msg }
+                    `);
+                },
+                loader: () => {
+                    const P = ['\\', '|', '/', '-'];
+                    let x = 0;
+                    this.state.loader = setInterval(() => {
+                        process.stdout.write(` Building ${ this.state.isApple ? '.ipa' : '.apk' } please wait... \r${P[x++]}`);
+                        x %= P.length;
+                    }, 250);
+                },
+                clearLoader: () => {
+                    clearInterval(this.state.loader);
                 }
             }
         }
-        this.state = { }
+        this.state = { 
+            loader: null
+        }
     }
 }
 module.exports = Utils;
